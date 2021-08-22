2bfa5a89f6a8a02dec1cc403520b1ee14f25919f
gabriel_alvesribeiro@hotmail.com <gabriel_alvesribeiro@hotmail.com>
2021-08-22 11:22:13 -0300

    First commit

---
+++ src/main/java/one/dio/personapi/controller/PersonController.java
@@ -0,0 +1,16 @@
+package one.dio.personapi.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+	
+	@GetMapping
+	public String getBook() {
+		return "API funcionando!";
+	}
+	
+}
