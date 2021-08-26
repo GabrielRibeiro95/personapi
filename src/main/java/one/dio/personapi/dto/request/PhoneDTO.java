e4892ba273a1e745038a74af6f8f7413baa65b95
gabriel_alvesribeiro@hotmail.com <gabriel_alvesribeiro@hotmail.com>
2021-08-25 21:48:54 -0300

    Improved person creation functionality

---
+++ src/main/java/one/dio/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,28 @@
+package one.dio.personapi.dto.request;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.dio.personapi.enums.PhoneType;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhoneDTO {
+	
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	private PhoneType type;
+	
+	@NotEmpty
+	@Size(min = 14, max = 15)
+	private String number;
+}
--- src/main/java/one/dio/personapi/service/PersonService.java
+++ src/main/java/one/dio/personapi/service/PersonService.java
@@ -3,21 +3,26 @@ package one.dio.personapi.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import one.dio.personapi.dto.MessageResponseDTO;
+import one.dio.personapi.dto.response.MessageResponseDTO;
+import one.dio.personapi.dto.request.PersonDTO;
 import one.dio.personapi.entity.Person;
+import one.dio.personapi.mapper.PersonMapper;
 import one.dio.personapi.repository.PersonRepository;
 
 @Service
 public class PersonService {
+	
 	private PersonRepository personRepository;
+	private final PersonMapper personMapper = PersonMapper.INSTANCE;
 	
 	@Autowired
 	public PersonService(PersonRepository personRepository) {
 		this.personRepository = personRepository;
 	}
 	
-	public MessageResponseDTO createPerson(Person person) {
-		Person savedPerson = personRepository.save(person);
+	public MessageResponseDTO createPerson(PersonDTO personDTO) {
+		Person personToSave = personMapper.toModel(personDTO);
+		Person savedPerson = personRepository.save(personToSave);
 		return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
 	}
 }
